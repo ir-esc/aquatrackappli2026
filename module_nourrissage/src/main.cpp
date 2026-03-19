6d63500208536a1329af1f7a9ea14efca8bc41f0
Eloise <eloise.mouret06@gmail.com>
2026-03-19 16:30:39 +0100

    Test 2.1 ESP32 : Allumer simplement les leds avec l'ESP32

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
+int led1 = 26;  //Attribue la led1 à la pin 26
+int led2 = 27;  //Attribue la led2 à la pin 27
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
+  pinMode(led1,OUTPUT);   //Initialise led1 en sortie
+  pinMode(led2,OUTPUT);   //Initialise led2 en sortie
+  Serial.begin(115200);   //Initialise la communication série à 115200 bauds
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello World!");
-  delay(1000);
+  digitalWrite(led1,HIGH);  //Allume la led1
+  digitalWrite(led2,HIGH);  //Allume la led2
 }
