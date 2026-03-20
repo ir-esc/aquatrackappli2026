25598a0b503f29fcb53a823825f9af65f2c2672a
Eloise <eloise.mouret06@gmail.com>
2026-03-20 09:37:34 +0100

    Test 3.3 Moteur : Faire un tour complet lorsque le bouton est enclenché avec l'ajout d'un état intermédiaire (problème : le moteur tourne mais il faut parfois appuyer 2 fois sur le bouton)

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -5,7 +5,7 @@ int IN1=26;
 int IN2=27;
 int CONTACTEUR=13;
 int BOUTON = 12; //pour simuler la demande de nourrissage
-int etat_moteur = 0;        //état du moteur à 0 donc à l'arrêt
+int etat_moteur = 2;        // état du moteur à 2 donc à l'arrêt sur le contacteur
 
 void setup(){
    pinMode(IN1,OUTPUT);
@@ -17,20 +17,30 @@ void setup(){
 }
 
 void loop() {
-    if (etat_moteur == 0) {
-        if (digitalRead(BOUTON) == LOW) {
-            etat_moteur = 1;
+    if (etat_moteur == 2) {     //si le moteur est arrêté sur le contacteur
+        if (digitalRead(BOUTON) == LOW) {   //et que le bouton est enclenché
+            etat_moteur = 0;        //alors le moteur se met en position prêt à démarrer
             digitalWrite(ENA,HIGH);
             digitalWrite(IN1,LOW);
             digitalWrite(IN2,HIGH);
+        }
     }
-}
 
-    if (etat_moteur == 1) {
-        if (digitalRead(CONTACTEUR) == LOW && digitalRead(BOUTON) == HIGH) {
-            etat_moteur = 0;
+    if (etat_moteur == 0) {     //si le moteur est prêt à démarrer
+        if (digitalRead(CONTACTEUR) == HIGH) {  //et que le contacteur est libéré
+            etat_moteur = 1;        //alors le moteur démarre
+            digitalWrite(ENA,HIGH);
+            digitalWrite(IN1,LOW);
+            digitalWrite(IN2,HIGH);
+        }
+    }
+
+    if (etat_moteur == 1) {     //si le moteur tourne
+        if (digitalRead(CONTACTEUR) == LOW) {   //et que le contacteur est enclenché
+            etat_moteur = 2;        //alors le moteur s'arrête sur le contacteur
             digitalWrite(IN1,LOW);
             digitalWrite(IN2,LOW);
         }
     }
+
 }
