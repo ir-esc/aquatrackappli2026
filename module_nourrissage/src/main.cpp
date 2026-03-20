494b0ac717f9e352e49d55c77cfa854053ce23c2
Eloise <eloise.mouret06@gmail.com>
2026-03-20 08:37:29 +0100

    Test 4.2 ESP32 : Allumer les leds à une intensité précise (une intensité différente pour chaque led)

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -3,23 +3,22 @@
 int led1 = 26;
 int led2 = 27;
 
-int pwmChannel = 0;	// Déclare le canal PWM à utiliser 
-int freq = 1000;	// Fréquence du signal PWM en Hz
-int resolution = 8;	// Résolution du PWM en bits (2^8 = 256, le PWM peut aller de 0 à 255)
+int pwmChannel1 = 0;	// Déclare le canal PWM à utiliser 
+int pwmChannel2 = 1;	// Déclare le canal PWM à utiliser (le canal est différent pour les deux leds aient des intensités différentes)
+int freq = 1000;
+int resolution = 8;
 
 void setup() {
  pinMode(led1,OUTPUT);
  pinMode(led2,OUTPUT);
- ledcSetup(pwmChannel, freq, resolution);	// Configure le canal PWM avec la fréquence et la résolution
- ledcAttachPin(led1, pwmChannel);	 // Attache la led1 au canal pwmChannel
- ledcAttachPin(led2, pwmChannel);	// Attache la led2 au canal pwmChannel
+ ledcSetup(pwmChannel, freq, resolution);	// Configure pwmChannel1 avec la fréquence et la résolution définies
+ ledcSetup(pwmChannel2, freq, resolution);	// Configure pwmChannel2 avec la fréquence et la résolution définies
+ ledcAttachPin(led1, pwmChannel1);	 // Attache la led1 au canal pwmChannel1
+ ledcAttachPin(led2, pwmChannel2);	// Attache la led2 au canal pwmChannel2
  Serial.begin(115200);
 }
 
 void loop(){
-   // Augmente progressivement la luminosité de des leds
-   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
-       ledcWrite(pwmChannel, dutyCycle);
-       delay(15);
-   }
+   ledcWrite(pwmChannel1, 64);	//Intensité de la led1 à 25%
+   ledcWrite(pwmChannel2, 192);	//Intensité de la led2 à 75%
 }
