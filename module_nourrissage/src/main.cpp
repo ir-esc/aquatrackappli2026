d9c5f6f24c6212c0e5e498da8019505bc9955fe5
Eloise <eloise.mouret06@gmail.com>
2026-03-20 09:44:25 +0100

    Test 4 Moteur : Faire tourner le moteur toutes les 10 secondes

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -4,56 +4,58 @@ int ENA=25;
 int IN1=26;
 int IN2=27;
 int CONTACTEUR=13;
-int BOUTON = 12;
 
 int etat_moteur = 2;
 unsigned long temps_rebond = 0;
+unsigned long temps_cycle = 0;
+
+const unsigned long periode = 10000; //10 secondes
 
 void setup(){
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(CONTACTEUR, INPUT_PULLUP);
-  pinMode(BOUTON, INPUT_PULLUP);
+ pinMode(IN1,OUTPUT);
+ pinMode(IN2,OUTPUT);
+ pinMode(ENA,OUTPUT);
+ pinMode(CONTACTEUR, INPUT_PULLUP);
 }
 
 void loop() {
-   // moteur arrêté sur contacteur
-   if (etat_moteur == 2) {
-       if (digitalRead(BOUTON) == LOW) {
-           digitalWrite(ENA,HIGH);
-           digitalWrite(IN1,LOW);
-           digitalWrite(IN2,HIGH);
-           etat_moteur = 0;
-       }
-   }
+  // moteur arrêté sur contacteur
+  if (etat_moteur == 2) {
+      if (millis() - temps_cycle > periode) {
+          digitalWrite(ENA,HIGH);
+          digitalWrite(IN1,LOW);
+          digitalWrite(IN2,HIGH);
+          etat_moteur = 0;
+      }
+  }
 
-   // quitter le contacteur
-   if (etat_moteur == 0) {
-       if (digitalRead(CONTACTEUR) == HIGH) {
-           etat_moteur = 1;
-       }
-   }
+  // quitter le contacteur
+  if (etat_moteur == 0) {
+      if (digitalRead(CONTACTEUR) == HIGH) {
+          etat_moteur = 1;
+      }
+  }
 
-   // moteur en rotation
-   if (etat_moteur == 1) {
-       if (digitalRead(CONTACTEUR) == LOW) {
-           temps_rebond = millis();
-           etat_moteur = 3;
-       }
-   }
+  // moteur en rotation
+  if (etat_moteur == 1) {
+      if (digitalRead(CONTACTEUR) == LOW) {
+          temps_rebond = millis();
+          etat_moteur = 3;
+      }
+  }
 
-   // anti rebond
-   if (etat_moteur == 3) {
-       if (millis() - temps_rebond > 30) {
-           if (digitalRead(CONTACTEUR) == LOW) {
-               digitalWrite(IN1,LOW);
-               digitalWrite(IN2,LOW);
-               etat_moteur = 2;
-           }
-           else {
-               etat_moteur = 1;
-           }
-       }
-   }
+  // anti rebond
+  if (etat_moteur == 3) {
+      if (millis() - temps_rebond > 30) {
+          if (digitalRead(CONTACTEUR) == LOW) {
+              digitalWrite(IN1,LOW);
+              digitalWrite(IN2,LOW);
+              temps_cycle = millis();  //reset du timer
+              etat_moteur = 2;
+          }
+          else {
+              etat_moteur = 1;
+          }
+      }
+  }
 }
