f9444b267bd67b8c4d01bc2be6becbfc6631ae9b
Eloise <eloise.mouret06@gmail.com>
2026-03-20 08:35:24 +0100

    Test 3 ESP32 : Allumer et éteindre des leds en fonction des touches du clavier avec l'ESP32

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -2,6 +2,7 @@
 
 int led1 = 26;
 int led2 = 27;
+char inChar;      //Définit une variable pour stocker les caractères reçus via le port série
 
 void setup() {
   pinMode(led1,OUTPUT);
@@ -10,16 +11,24 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(led1,HIGH);
-  digitalWrite(led2,LOW);
-  delay(1000);    //Attendre une seconde
-  digitalWrite(led1,HIGH);
-  digitalWrite(led2,HIGH);
-  delay(1000);    //Attendre une seconde
-  digitalWrite(led1,LOW);
-  digitalWrite(led2,HIGH);
-  delay(1000);      //Attendre une seconde
-  digitalWrite(led1,LOW);
-  digitalWrite(led2,LOW);
-  delay(1000);      //Attendre une seconde
+ if (Serial.available()){   //Juge si les données ont été reçues ou non
+   inChar = Serial.read();    //Lit un caractère
+   Serial.print("Character received:");   //Affiche "Character received:" dans le moniteur série
+   Serial.println(inChar);    //Affiche le caractère reçu dans le moniteur série
+ }
+ if (inChar == '1'){
+   digitalWrite(led1, HIGH);    //Lorsque le caractère '1' est reçu la led1 s'allume
+ }
+
+ if (inChar == '2'){
+   digitalWrite(led1, LOW);   //Lorsque le caractère '2' est reçu la led1 s'éteint
+ }
+
+ if (inChar == '3'){
+   digitalWrite(led2, HIGH);    //Lorsque le caractère '3' est reçu la led2 s'allume
+ }
+
+ if (inChar == '4'){
+   digitalWrite(led2, LOW);   //Lorsque le caractère '4' est reçu la led2 s'éteint
+ }
 }
