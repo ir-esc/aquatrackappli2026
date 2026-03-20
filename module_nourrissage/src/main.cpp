a61124e6e9884ec4cd44bdf12b8f627efe84a70e
Eloise <eloise.mouret06@gmail.com>
2026-03-20 09:01:42 +0100

    Test 2 Resistance : Tester la résistance pull up avec la led intégrée de l'ESP32

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,12 +1,23 @@
 #include <Arduino.h>
 
+int button = 13;
+int led = 2; //led intégrée sur l’ESP32
+
 void setup() {
-  pinMode(13, INPUT_PULLUP);	//Résistance interne de l'ESP32
+  pinMode(button, INPUT_PULLUP); // Active la résistance pull-up interne
+  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);
   Serial.begin(115200);
 }
 
 void loop() {
- bool buttonStatus = digitalRead(13);	  // Lit l'état de la broche 13 et stocke le résultat dans la variable buttonStatus (HIGH = bouton non pressé, LOW = bouton pressé)
- Serial.println(buttonStatus);	//Envoie l'état du bouton au moniteur série (affiche 1 pour HIGH, 0 pour LOW)
- delay(100);
+  if (digitalRead(button) == LOW){ //Rappel : En pull up LOW = bouton enclenché alors qu’en pull down HIGH = bouton enclenché
+    digitalWrite(led, HIGH);
+    Serial.println("LED ON");
+    delay(100);
+  } else {
+    digitalWrite(led, LOW);
+    Serial.println("LED OFF");
+    delay(100);
+  }
 }
