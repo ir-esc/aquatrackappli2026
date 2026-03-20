c269c77d91e63f444e47978faf37991a88058486
Eloise <eloise.mouret06@gmail.com>
2026-03-20 08:56:14 +0100

    Test 1 Resistance : Tester la résistance pull up avec la résistance interne de l’ESP32 (lorsque le bouton n’est pas enclenché des 1 sont affichés et lorsqu’il est enclenché des 0 sont affichés)

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,49 +1,12 @@
 #include <Arduino.h>
 
-int ENA = 25;
-int IN1 = 26;
-int IN2 = 27;
-
-int pwmChannel = 0;
-int freq = 1000;
-int resolution = 8;
-
 void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  ledcSetup(pwmChannel, freq, resolution);
-  ledcAttachPin(ENA, pwmChannel);
+  pinMode(13, INPUT_PULLUP);	//Résistance interne de l'ESP32
   Serial.begin(115200);
 }
 
 void loop() {
-  // Sens 1
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  ledcWrite(pwmChannel, 127);
-  Serial.println("Sens 1");
-  delay(1000);
-
-  // Sens 2
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  ledcWrite(pwmChannel, 127);
-  Serial.println("Sens 2");
-  delay(1000);
-
-
-  // Arret
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  ledcWrite(pwmChannel, 0);
-  Serial.println("Arret");
-  delay(1000);
-
-  //Vitesse
-  for (int vitesse = 0; vitesse <= 255; vitesse++) {
-    ledcWrite(pwmChannel, vitesse);
-    Serial.println("Vitesse");
-    delay(15);
-  }
+ bool buttonStatus = digitalRead(13);	  // Lit l'état de la broche 13 et stocke le résultat dans la variable buttonStatus (HIGH = bouton non pressé, LOW = bouton pressé)
+ Serial.println(buttonStatus);	//Envoie l'état du bouton au moniteur série (affiche 1 pour HIGH, 0 pour LOW)
+ delay(100);
 }
