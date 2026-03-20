9d689723c652d5fe8e88daefcdb2aacd8d5952cc
Eloise <eloise.mouret06@gmail.com>
2026-03-20 08:54:51 +0100

    Test 2.2 L298N : Simulation de la vitesse du moteur avec la carte L298N (test avec les 3 leds)

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 
-int ENA = 26;	//La borne ENA de la carte L298N est simulée par une led
-int IN1 = 27;
+int ENA = 25;
+int IN1 = 26;
+int IN2 = 27;
 
 int pwmChannel = 0;
 int freq = 1000;
@@ -9,6 +10,7 @@ int resolution = 8;
 
 void setup() {
   pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
   ledcSetup(pwmChannel, freq, resolution);
   ledcAttachPin(ENA, pwmChannel);
@@ -16,15 +18,32 @@ void setup() {
 }
 
 void loop() {
-  // Marche
+  // Sens 1
   digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   ledcWrite(pwmChannel, 127);
-  Serial.println("Marche");
+  Serial.println("Sens 1");
   delay(1000);
 
-  // Arrêt
+  // Sens 2
   digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  ledcWrite(pwmChannel, 127);
+  Serial.println("Sens 2");
+  delay(1000);
+
+
+  // Arret
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
   ledcWrite(pwmChannel, 0);
   Serial.println("Arret");
   delay(1000);
+
+  //Vitesse
+  for (int vitesse = 0; vitesse <= 255; vitesse++) {
+    ledcWrite(pwmChannel, vitesse);
+    Serial.println("Vitesse");
+    delay(15);
+  }
 }
