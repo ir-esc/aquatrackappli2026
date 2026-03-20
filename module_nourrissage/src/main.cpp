191c64ae047f15819af7a0430e852ce539296b20
Eloise <eloise.mouret06@gmail.com>
2026-03-20 09:39:53 +0100

    Test 3.4 Moteur : Faire un tour complet lorsque le bouton est enclenché avec l'ajout d'un delai anti-rebond

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -8,39 +8,39 @@ int BOUTON = 12; //pour simuler la demande de nourrissage
 int etat_moteur = 2;        // état du moteur à 2 donc à l'arrêt sur le contacteur
 
 void setup(){
-   pinMode(IN1,OUTPUT);
-   pinMode(IN2,OUTPUT);
-   pinMode(ENA,OUTPUT);
-   pinMode(CONTACTEUR, INPUT_PULLUP);
-   pinMode(BOUTON, INPUT_PULLUP);
-   Serial.begin(115200);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(CONTACTEUR, INPUT_PULLUP);
+  pinMode(BOUTON, INPUT_PULLUP);
+  Serial.begin(115200);
 }
 
 void loop() {
-    if (etat_moteur == 2) {     //si le moteur est arrêté sur le contacteur
-        if (digitalRead(BOUTON) == LOW) {   //et que le bouton est enclenché
-            etat_moteur = 0;        //alors le moteur se met en position prêt à démarrer
-            digitalWrite(ENA,HIGH);
-            digitalWrite(IN1,LOW);
-            digitalWrite(IN2,HIGH);
-        }
-    }
+   if (etat_moteur == 2) {     //si le moteur est arrêté sur le contacteur
+       if (digitalRead(BOUTON) == LOW) {   //et que le bouton est enclenché
+           etat_moteur = 0;        //alors le moteur se met en position prêt à démarrer
+           digitalWrite(ENA,HIGH);
+           digitalWrite(IN1,LOW);
+           digitalWrite(IN2,HIGH);
+       }
+   }
 
-    if (etat_moteur == 0) {     //si le moteur est prêt à démarrer
-        if (digitalRead(CONTACTEUR) == HIGH) {  //et que le contacteur est libéré
-            etat_moteur = 1;        //alors le moteur démarre
-            digitalWrite(ENA,HIGH);
-            digitalWrite(IN1,LOW);
-            digitalWrite(IN2,HIGH);
-        }
-    }
+   if (etat_moteur == 0) {     //si le moteur est prêt à démarrer
+       if (digitalRead(CONTACTEUR) == HIGH) {  //et que le contacteur est libéré
+           digitalWrite(ENA,HIGH);
+           digitalWrite(IN1,LOW);
+           digitalWrite(IN2,HIGH);
+           delay(50);		//delai anti-rebond
+           etat_moteur = 1;        //alors le moteur démarre
+       }
+   }
 
-    if (etat_moteur == 1) {     //si le moteur tourne
-        if (digitalRead(CONTACTEUR) == LOW) {   //et que le contacteur est enclenché
-            etat_moteur = 2;        //alors le moteur s'arrête sur le contacteur
-            digitalWrite(IN1,LOW);
-            digitalWrite(IN2,LOW);
-        }
-    }
-
+   if (etat_moteur == 1) {     //si le moteur tourne
+       if (digitalRead(CONTACTEUR) == LOW) {   //et que le contacteur est enclenché
+           etat_moteur = 2;        //alors le moteur s'arrête sur le contacteur
+           digitalWrite(IN1,LOW);
+           digitalWrite(IN2,LOW);
+       }
+   }
 }
