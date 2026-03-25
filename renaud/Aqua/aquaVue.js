3b7f52a9916f36a569b4791756935b962f9b5070
Duaner23 <erwanrenaud23@gmail.com>
2026-03-25 10:25:27 +0100

    Ajout de toutes les valeurs et images

--- renaud/Aqua/aquaVue.js
+++ renaud/Aqua/aquaVue.js
@@ -1,7 +1,12 @@
 webix.ready(function(){
+  /* 
+  envoie une requête POST à l'API pour se connecter avec les informations d'identification
+  mais ne sert à rien pour le moment car tout les aquariums sont deja affichés dans l'API sans besoin de se connecter
+  il faudrait ajouter une vérification de connexion pour afficher les données seulement si l'utilisateur est connecté
   webix.ajax()
     .headers({"Content-Type":"application/json"})
     .post("//aquatrackapi.ir.lan/log",JSON.stringify({"email": "Alex@ir.lan","motdepasse": "Alex1234"}));
+    */
   webix.ui({
     view: "scrollview",
     scroll: "y",
@@ -11,10 +16,12 @@ webix.ready(function(){
           // affiche les données de l'API dans une datatable
           view:"datatable",
           columns:[
-            { id:"id", header:"ID", fillspace:true },
+            { id:"media_id", header:"Image", width:50, template:"<img src='//aquatrackapi.ir.lan/aqr/#media_id#' width='50' height='50'>"},
             { id:"nom", header:"Nom", fillspace:true },
-            { id:"volume", header:"Volume", fillspace:true },
-            { id:"date", header:"Date de creation", fillspace:true }
+            { id:"user_id", header:"Propriétaire", width:90 },
+            { id:"acces", header:"Accès", width:90 },
+            { id:"volume", header:"Volume", width:90 },
+            { id:"date", header:"Date de creation", width:155 }
           ],
           // récupère les données de l'API avec l'URL spécifiée
           url:function(params){
