ff613c4eb39b861396bee5459baa3ab6e01b7cf3
Manteau Yasmine <manteau@b303-16-lm>
2026-04-03 09:17:21 +0200

    Test de la connexion Wifi de l'ESP32

--- module_pH/src/main.cpp
+++ module_pH/src/main.cpp
@@ -1,82 +1,35 @@
 #include <Arduino.h>
-#include <Preferences.h>
+#include <WiFi.h>
 
-#define PH_PIN 34
-
-Preferences prefs;
-
-float voltageAtPH7;
-float slope;
-
-float readVoltage() {
-    int adc = analogRead(PH_PIN);
-    return adc * 3.3 / 4095.0;
-}
-
-void saveCalibration() {
-    prefs.begin("phCal", false);
-    prefs.putFloat("v7", voltageAtPH7);
-    prefs.putFloat("slope", slope);
-    prefs.end();
-}
-
-void loadCalibration() {
-    prefs.begin("phCal", true);
-    voltageAtPH7 = prefs.getFloat("v7", 1.45);
-    slope = prefs.getFloat("slope", -0.17);
-    prefs.end();
-}
+const char* ssid = "IR";
+const char* password = "G00dWave$";
 
 void setup() {
     Serial.begin(115200);
-    analogReadResolution(12);
-    analogSetAttenuation(ADC_11db);
+    delay(1000);
 
-    loadCalibration();
+    Serial.print("Connexion au WiFi ");
+    Serial.println(ssid);
 
-    Serial.println("=== pH Sensor Ready ===");
-    Serial.println("Tapez 7 pour calibrer pH7");
-    Serial.println("Tapez 4 pour calibrer pH4");
-    Serial.println("Tapez R pour reset");
+    WiFi.begin(ssid, password);
+
+
+    int retry = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+        retry++;
+        if(retry > 20){
+            Serial.println("\nImpossible de se connecter au WiFi.");
+            return;
+        }
+    }
+
+    Serial.println("\nConnecté !");
+    Serial.print("Adresse IP : ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
 
-    if (Serial.available()) {
-        char cmd = Serial.read();
-
-        if (cmd == '7') {
-            Serial.println("Placez la sonde dans solution pH 7...");
-            delay(5000);
-            voltageAtPH7 = readVoltage();
-            saveCalibration();
-            Serial.println("Calibration pH7 OK");
-        }
-
-        if (cmd == '4') {
-            Serial.println("Placez la sonde dans solution pH 4...");
-            delay(5000);
-            float voltage4 = readVoltage();
-            slope = (voltage4 - voltageAtPH7) / (4.0 - 7.0);
-            saveCalibration();
-            Serial.println("Calibration pH4 OK");
-        }
-
-        if (cmd == 'R') {
-            voltageAtPH7 = 1.45;
-            slope = -0.17;
-            saveCalibration();
-            Serial.println("Calibration reset");
-        }
-    }
-
-    float voltage = readVoltage();
-    float pH = 7.0 + (voltage - voltageAtPH7) / slope;
-
-    Serial.print("Voltage: ");
-    Serial.print(voltage, 3);
-    Serial.print(" V | pH: ");
-    Serial.println(pH, 2);
-
-    delay(1000);
 }
