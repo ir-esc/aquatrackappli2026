c5ea418c36f474e1d753deaa795f462f7c7d0117
Manteau Yasmine <manteau@b303-16-lm>
2026-04-03 09:12:57 +0200

    Premier test avec le capteur pH

--- module_pH/src/main.cpp
+++ module_pH/src/main.cpp
@@ -1,12 +1,29 @@
 #include <Arduino.h>
 
+#define PH_PIN 34
+
+float voltageAtPH7 = 1.45; // mesure réelle de la tension
+float slope = -0.17;    // V/pH à 25°C
+
 void setup() {
-    // put your setup code here, to run once:
     Serial.begin(115200);
+    analogReadResolution(12);
+    analogSetAttenuation(ADC_11db);
+    Serial.println("SEN0161 pH sensor ready");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    Serial.println("Hello World!");
+    int adc = analogRead(PH_PIN);
+    float voltage = adc * 3.3 / 4095.0;
+
+    float pH = 7.0 + (voltage - voltageAtPH7) / slope;
+
+    Serial.print("ADC = ");
+    Serial.print(adc);
+    Serial.print(" | Voltage = ");
+    Serial.print(voltage, 3);
+    Serial.print(" V | pH = ");
+    Serial.println(pH, 2);
+
     delay(1000);
 }
