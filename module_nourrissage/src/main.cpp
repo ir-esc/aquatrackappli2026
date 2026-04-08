b21cbff581db29e1a15a14525ece539c42264887
Eloise <eloise.mouret06@gmail.com>
2026-04-08 10:05:59 +0200

    Test Wifi : test de la connexion Wifi de l'ESP32

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,61 +1,23 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "wifi_config.h"
 
-int ENA=25;
-int IN1=26;
-int IN2=27;
-int CONTACTEUR=13;
+void setup() {
+   Serial.begin(115200);
+   delay(1000);
 
-int etat_moteur = 2;
-unsigned long temps_rebond = 0;
-unsigned long temps_cycle = 0;
+   WiFi.begin(ssid, password);
+   Serial.print("\nConnecting");
 
-const unsigned long periode = 10000; //10 secondes
-
-void setup(){
- pinMode(IN1,OUTPUT);
- pinMode(IN2,OUTPUT);
- pinMode(ENA,OUTPUT);
- pinMode(CONTACTEUR, INPUT_PULLUP);
+   // Attente de la connexion
+   while (WiFi.status() != WL_CONNECTED) {
+       Serial.print(".");
+       delay(1000);
 }
 
-void loop() {
-  // moteur arrêté sur contacteur
-  if (etat_moteur == 2) {
-      if (millis() - temps_cycle > periode) {
-          digitalWrite(ENA,HIGH);
-          digitalWrite(IN1,LOW);
-          digitalWrite(IN2,HIGH);
-          etat_moteur = 0;
-      }
-  }
-
-  // quitter le contacteur
-  if (etat_moteur == 0) {
-      if (digitalRead(CONTACTEUR) == HIGH) {
-          etat_moteur = 1;
-      }
-  }
-
-  // moteur en rotation
-  if (etat_moteur == 1) {
-      if (digitalRead(CONTACTEUR) == LOW) {
-          temps_rebond = millis();
-          etat_moteur = 3;
-      }
-  }
-
-  // anti rebond
-  if (etat_moteur == 3) {
-      if (millis() - temps_rebond > 30) {
-          if (digitalRead(CONTACTEUR) == LOW) {
-              digitalWrite(IN1,LOW);
-              digitalWrite(IN2,LOW);
-              temps_cycle = millis();  //reset du timer
-              etat_moteur = 2;
-          }
-          else {
-              etat_moteur = 1;
-          }
-      }
-  }
+  Serial.println("\nConnected to the Wifi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
+
+void loop() {}
