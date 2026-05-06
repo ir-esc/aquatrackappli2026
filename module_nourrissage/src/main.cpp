1e54bd7fae30adaaffaa090f188c8db9cdbff53d
Eloise <eloise.mouret06@gmail.com>
2026-05-06 08:17:26 +0200

    Test API : test de la connexion et de l'envoi d'une observation à l'API +  séparation de la partie connexion wifi

--- module_nourrissage/src/main.cpp
+++ module_nourrissage/src/main.cpp
@@ -1,102 +1,83 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "time.h"
+#include <time.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "wifi_config.h"
-
-int ENA = 25;
-int IN1 = 26;
-int IN2 = 27;
-int CONTACTEUR = 13;
-
-int etat_moteur = 2;
-unsigned long temps_rebond = 0;
-
-int heure_declenchement = 14;
-int minute_declenchement = 32;
-
-bool deja_declenche = false;    // permet d'éviter plusieurs déclenchements
-
-const char* ntpServer = "pool.ntp.org";
-const long  gmtOffset_sec = 3600 * 1;
-const int   daylightOffset_sec = 3600 * 1;  //Heure d'été
+#include "WifiManager.h"
 
 void setup() {
-   pinMode(IN1, OUTPUT);
-   pinMode(IN2, OUTPUT);
-   pinMode(ENA, OUTPUT);
-   pinMode(CONTACTEUR, INPUT_PULLUP);
-   Serial.begin(115200);
-   delay(1000);
+    Serial.begin(115200);
 
-   WiFi.begin(ssid, password);
-   Serial.println("\nConnecting");
+    connexionWifi(ssid, password);
+    configTime(3600, 3600, "pool.ntp.org");
+}
 
-   while (WiFi.status() != WL_CONNECTED) {
-       Serial.print(".");
-       delay(100);
-   }
+void loginApi() {
+    HTTPClient http;
 
-   Serial.println("\nConnected to the Wifi network");
-   Serial.println(WiFi.localIP());
+    http.begin("http://aquatrackapi.ir.lan/log");
 
-   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    http.addHeader("Content-Type", "application/json");
+
+    String body = "{";
+    body += "\"email\":\"Alex@ir.lan\",";
+    body += "\"motdepasse\":\"Alex1234\"";
+    body += "}";
+
+    int code = http.POST(body);
+
+    Serial.print("Login code: ");
+    Serial.println(code);
+    Serial.println(http.getString());
+
+    http.end();
+}
+
+void envoiAPI() {
+    struct tm timeinfo;
+
+    getLocalTime(&timeinfo);
+    char buffer[20];
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &timeinfo);
+    String date = String(buffer);
+
+    HTTPClient http;
+
+    //URL complète
+    String url = "http://aquatrackapi.ir.lan/aqr/116/obs";
+    http.begin(url);
+
+    //Header JSON
+    http.addHeader("Content-Type", "application/json");
+
+    //Corps de la requète
+    StaticJsonDocument<200> doc;
+    doc["texte"] = "test nourrissage";
+    doc["date"] = date;
+
+    String requestBody;
+    serializeJson(doc, requestBody);
+
+    //Envoi POST
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0) {
+        Serial.print("Code HTTP : ");
+        Serial.println(httpResponseCode);
+
+        String response = http.getString();
+        Serial.println("Réponse : " + response);
+    } else {
+        Serial.print("Erreur requète : ");
+        Serial.println(httpResponseCode);
+    }
+
+    http.end();
 }
 
 void loop() {
-   struct tm timeinfo;
-
-   if (!getLocalTime(&timeinfo)) {
-       Serial.println("Failed to obtain time");
-       return;
-   }
-
-   Serial.println(&timeinfo, "%H:%M:%S");
-
-   // moteur arrêté sur contacteur
-   if (etat_moteur == 2) {
-       if (timeinfo.tm_hour == heure_declenchement && timeinfo.tm_min  == minute_declenchement && !deja_declenche) {
-           digitalWrite(ENA, HIGH);
-           digitalWrite(IN1, LOW);
-           digitalWrite(IN2, HIGH);
-
-           etat_moteur = 0;
-           deja_declenche = true;
-       }
-   }
-
-   // reset du déclenchement
-   if (timeinfo.tm_min != minute_declenchement) {
-       deja_declenche = false;
-   }
-
-   // quitter le contacteur
-   if (etat_moteur == 0) {
-       if (digitalRead(CONTACTEUR) == HIGH) {
-           etat_moteur = 1;
-       }
-   }
-
-   // moteur en rotation
-   if (etat_moteur == 1) {
-       if (digitalRead(CONTACTEUR) == LOW) {
-           temps_rebond = millis();
-           etat_moteur = 3;
-       }
-   }
-
-   // anti rebond
-   if (etat_moteur == 3) {
-       if (millis() - temps_rebond > 30) {
-           if (digitalRead(CONTACTEUR) == LOW) {
-               digitalWrite(IN1, LOW);
-               digitalWrite(IN2, LOW);
-               digitalWrite(ENA, LOW);
-
-               etat_moteur = 2;
-           } else {
-               etat_moteur = 1;
-           }
-       }
-   }
-   delay(100);
+    loginApi();
+    envoiAPI();
+    delay(30000);
 }
