be3207aa72657658b9cea888157c789e3a92ead4
Duaner23 <erwanrenaud23@gmail.com>
2026-06-01 14:22:48 +0200

    le bouton voir recupere l'id et le stock en local

--- appli/js/views/aquariums.js
+++ appli/js/views/aquariums.js
@@ -45,7 +45,8 @@ function getVueAquariums() {
             view: "button", value: "Voir", css: "webix_secondary", height: 50, click: function () {
               var selected = $$("aquaTable").getSelectedId(true).join();
               if (selected) {
-                window.location.href = "../Mesures/mesures.html?id=" + selected;
+                //window.location.href = "../Mesures/mesures.html?id=" + selected;
+                webix.storage.local.put("aquarium_id", selected);
               }
               else {
                 webix.alert("Veuillez sélectionner un aquarium");
